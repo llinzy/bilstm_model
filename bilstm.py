6e22bf9bae7f37adedf1da6e6618eed46a2d4ee8
Kysha <33337655+llinzy@users.noreply.github.com>
2020-09-24 21:51:29 -0400

    Update bilstm.py
    update

--- bilstm.py
+++ bilstm.py
@@ -210,15 +210,20 @@ df_encoded_=df_encoded[['actual_class_','predicted_class_','text2']].reset_index
 
 df_encoded_.columns=['id', 'actual_class_', 'predicted_class_', 'words']
 
-st.write('Predict Skill Category with Bidirectional Long-term Short-term Memory (biLSTM)')
 
-st.subheader('Prediction Result Loss & Accuracy Report')
-st.write(output_df)
+st.write('Prediction Results for Binary Bidirectional Long-term Short-term Memory (BiLSTM)')
 
-st.subheader('Results Data')
+st.subheader('Prediction Results Loss & Accuracy Report')
+#st.write(output_df.iloc[:,1:])
+
+st.subheader('Predicted Skill Category for each ID')
 df_cat_default_type=st.selectbox('Select ID', list(df_encoded_.id.unique()))
                           
 df_cat_df=df_encoded_[df_encoded_.id==df_cat_default_type]
 
-st.write(df_cat_df.iloc[:,:2])     
-st.write(df_cat_df.iloc[:,3])
+st.write(df_cat_df.iloc[:,1:4])
+         
+st.subheader('Words Matched to Predicted Skill Category')
+st.write(pd.concat([pd.Series(list(range(1,len(df_cat_df.iloc[:,4])+1)),name='index_'), 
+           pd.Series(df_cat_df.iloc[:,4],name='word').explode()],
+          axis=1))
